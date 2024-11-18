424d17933a587e954b1a33ee8aa1efc35c0b7897
toertchen273 <85648976+toertchen273@users.noreply.github.com>
2024-11-18 09:42:10 +0400

    changes

--- integration/connection.js
+++
@@ -1,3 +0,0 @@
-import { Connection } from "@solana/web3.js";
-export const connection = new Connection("https://light-wider-bird.solana-mainnet.quiknode.pro/0fa02f9109ca0c0e8a990f73ca03664b2d3e4138", 'confirmed');
-
